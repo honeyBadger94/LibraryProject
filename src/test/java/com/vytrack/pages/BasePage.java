62ca225178ff24eaf3fc208c6500b0bff2acde57
honeybadger94 <cihatozcan1903@hotmail.com>
2021-01-31 14:11:42 -0600

    first commit

---
+++ src/test/java/com/vytrack/pages/BasePage.java
@@ -0,0 +1,24 @@
+package com.vytrack.pages;
+
+import com.vytrack.utils.BrowserUtils;
+import com.vytrack.utils.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public abstract class BasePage {
+
+
+
+    public BasePage() {
+
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
+}
---
+++ src/test/java/com/vytrack/pages/LoginPage.java
@@ -0,0 +1,13 @@
+package com.vytrack.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage {
+
+    @FindBy(id = "inputEmail")
+    WebElement inputEmail;
+
+
+
+}
